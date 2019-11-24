3338612543ab20233e8746ec158e30a900f2e515
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-11-24 15:39:16 +0300

    Доделала задание

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ const tableEntry = function (n, t, u, c, ct) {
 
 let data = {
   table: [],
+  sort: '',
   summ: 0,
+  errorName: false,
+  errorCost: false,
   newEntry: {
     name: "",
     type: "",
@@ -18,26 +21,59 @@ let data = {
   }
 };
 
+const dynamicSort = function (property) {
+  var sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    return result * sortOrder;
+  }
+}
+
 const app = new Vue({
   el: '#app',
   data: data,
   methods: {
     addLine: () => {
-      data.table.push(new tableEntry(
-        data.newEntry.name,
-        data.newEntry.type,
-        data.newEntry.unit,
-        data.newEntry.count,
-        data.newEntry.cost
-      ));
-      app.getSumm();
-      data.newEntry = {
-        name: "",
-        type: "",
-        unit: "Час",
-        count: "",
-        cost: ""
-      };
+      if (!app.isUnic(data.newEntry.name) || data.newEntry.name === "") {
+        data.errorName = true;
+      } else {
+        data.errorName = false;
+      }
+      if (data.newEntry.cost <= 0) {
+        data.errorCost = true;
+      } else {
+        data.errorCost = false;
+      }
+      if (data.errorName === false && data.errorCost === false) {
+        data.table.push(new tableEntry(
+          data.newEntry.name,
+          data.newEntry.type,
+          data.newEntry.unit,
+          data.newEntry.count,
+          data.newEntry.cost
+        ));
+        app.getSumm();
+        data.newEntry = {
+          name: "",
+          type: "",
+          unit: "Час",
+          count: "",
+          cost: ""
+        };
+      }
+    },
+    isUnic: (name) => {
+      let result = true;
+      data.table.forEach(function (elem) {
+        if (name === elem.name) {
+          result = false;
+        }
+      });
+      return result;
     },
     moveLine: (cmd, elem) => {
       let index = -1;
@@ -46,15 +82,40 @@ const app = new Vue({
           index = i;
         }
       }
-      switch(cmd) {
+      switch (cmd) {
         case 'del': {
-            if (index > -1) {
-                data.table.splice(index, 1);
-            }
-            break;
+          if (index > -1) {
+            data.table.splice(index, 1);
+          }
+          break;
         }
-    };
+        case 'up': {
+          if (index - 1 > -1) {
+            let tmp = data.table[index];
+            data.table[index] = data.table[index - 1];
+            data.table[index - 1] = tmp;
+          }
+          break;
+        }
+        case 'down': {
+          if (index + 1 < data.table.length) {
+            let tmp = data.table[index];
+            data.table[index] = data.table[index + 1];
+            data.table[index + 1] = tmp;
+          }
+          break;
+        }
+      };
       app.getSumm();
+    },
+    doSort: (key) => {
+      if (data.sort.indexOf(key) !== -1) {
+        if (data.sort.indexOf('-') === -1) {
+          key = '-' + key;
+        }
+      }
+      data.sort = key;
+      data.table.sort(dynamicSort(key));
     },
     init: () => {
       data.table.push(new tableEntry(
@@ -72,11 +133,12 @@ const app = new Vue({
         10000
       ));
       app.getSumm();
+      app.doSort('name');
     },
     getSumm: () => {
       data.summ = 0;
-      data.table.forEach(function(elem) {
-        data.summ += elem.cost;
+      data.table.forEach(function (elem) {
+        data.summ += Number(elem.cost);
       })
     }
   }
